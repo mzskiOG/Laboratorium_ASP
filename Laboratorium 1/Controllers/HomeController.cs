1cba616672fc5e6c4fdad5caa80ee13c41c66ef5
mzskiOG <128607570+mzskiOG@users.noreply.github.com>
2023-11-06 19:03:16 +0100

    https://record-it.gitbook.io/programowanie-w-srodowisku-asp.net/laboratorium-1

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -28,5 +28,58 @@ namespace Laboratorium_1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult About(Operator op)
+        {
+            ViewBag.Op = op;
+            return View();
+        }
+         public enum Operator
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+        public IActionResult Calculator(Operator op, double? a, double? b)
+        {
+            if (!a.HasValue || !b.HasValue)
+            {
+                ViewBag.Error = "Invalid input. Please enter valid numbers.";
+                return View();
+            }
+
+            double result = 0;
+
+            switch (op)
+            {
+                case Operator.Add:
+                    result = a.Value + b.Value;
+                    break;
+                case Operator.Mul:
+                    result = a.Value * b.Value;
+                    break;
+                case Operator.Sub:
+                    result = a.Value - b.Value;
+                    break;
+                case Operator.Div:
+                    if (b.Value != 0)
+                    {
+                        result = a.Value / b.Value;
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Division by zero is not allowed.";
+                        return View();
+                    }
+                    break;
+                default:
+                    ViewBag.Error = "Invalid operator. Please select a valid operator.";
+                    return View();
+            }
+
+            ViewBag.Expression = $"{a} {op} {b}";
+            ViewBag.Result = $"Wynik działania dla {ViewBag.Expression} = {result}";
+            return View();
+        }
+
     }
 }
